d5a261a7e2cead2a5e128b3cd5674657bd4bfb3c
ec-ayrton <ec.ayrton@gmail.com>
2020-09-08 15:26:24 -0300

    first commit

---
+++ src/main/java/com/ayrton/gerenpersoapi/GerenpersoapiApplication.java
@@ -0,0 +1,13 @@
+package com.ayrton.gerenpersoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenpersoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenpersoapiApplication.class, args);
+	}
+
+}
